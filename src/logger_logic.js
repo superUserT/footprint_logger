c12369a206f6665d0606e5f3f0c6baaba7c00a31
superUserT <rantshothabisomail@gmail.com>
2025-06-30 00:03:59 +0200

    added dropdown

--- src/logger_logic.js
+++ src/logger_logic.js
@@ -1,1 +1,13 @@
-var nums = [1, 2, 3];
+"use strict";
+const email = "{{ email }}";
+function showActivityDetails(activity) {
+    document.querySelectorAll(".activity-details").forEach((el) => {
+        el.style.display = "none";
+    });
+    if (activity) {
+        const detailsElement = document.getElementById(`${activity}Details`);
+        if (detailsElement) {
+            detailsElement.style.display = "block";
+        }
+    }
+}
--- src/logger_logic.ts
+++ src/logger_logic.ts
@@ -1,3 +1,14 @@
-function pow(x: number, y: number): number {
-  return Math.pow(x, y);
+const email: string = "{{ email }}";
+
+function showActivityDetails(activity: any): void {
+  document.querySelectorAll(".activity-details").forEach((el) => {
+    (el as HTMLElement).style.display = "none";
+  });
+
+  if (activity) {
+    const detailsElement = document.getElementById(`${activity}Details`);
+    if (detailsElement) {
+      detailsElement.style.display = "block";
+    }
+  }
 }
